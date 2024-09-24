83583664ef61ddd4ab1cd41f8dbce5b8e92d2566
Md Ammar <ammaratif559@gmail.com>
2024-09-24 19:38:14 +0530

    initial Commit
    Adding project files

---
+++ scrape.py
@@ -0,0 +1,7 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.options import Options
+
+def scrape_website(website):
+    pass
+
+scrape_website("https://www.techwithtim.net/")
